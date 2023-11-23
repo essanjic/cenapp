10e5c3c55f656b28538602038e68668f9420de5b
Ael <essanjic@gmail.com>
2023-11-22 19:40:38 -0500

    Adding more logic for security and change properties

--- src/main/java/com/group6/cenapp/repository/UserInfoRepository.java
+++ src/main/java/com/group6/cenapp/repository/UserInfoRepository.java
@@ -1,13 +1,13 @@
 package com.group6.cenapp.repository;
 
 
-import com.group6.cenapp.model.Entity.User;
+import com.group6.cenapp.model.Entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserInfoRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
 }
