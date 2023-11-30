809f9fc126593f1ecd2c8e73abb4a2272e13dcfa
Esteban Jiménez <esteban.jimenez@mercadolibre.com.co>
2023-11-30 09:45:45 -0500

    fix change name of project in pom

--- src/main/java/com/group6/cenapp/cenapp.java
+++ src/main/java/com/group6/cenapp/cenapp.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.group6.cenapp"})
 public class cenapp{
 
     public static void main(String[] args) {
